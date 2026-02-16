0c36c66013bba1563d7bfd3e1798c2cae75468e3
Your Name <you@example.com>
2026-02-16 11:24:30 -0500

    a

---
+++ Untitled-1.sh
@@ -0,0 +1,13 @@
+sudo fallocate -l 40G /ssd/swapfile2
+sudo chmod 600 /ssd/swapfile2
+sudo mkswap /ssd/swapfile2
+sudo swapon /ssd/swapfile2
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['']"
+
+kwriteconfig6 --file kglobalshortcutsrc --group kwin --key "Switch One Desktop Up" "none,none,Switch One Desktop Up"
+kwriteconfig6 --file kglobalshortcutsrc --group kwin --key "Switch One Desktop Down" "none,none,Switch One Desktop Down"
+
+# Reload the shortcut daemon to apply changes
+kquitapp6 kglobalaccel && sleep 2s && kglobalaccel6 &
